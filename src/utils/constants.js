e654921aadb962cacee95eba00b9f0a04786eaaa
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2024-11-29 20:47:02 +0530

    netlifi1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://devtinder-1.netlify.app";
+export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "https://devtinder-1.netlify.app";
